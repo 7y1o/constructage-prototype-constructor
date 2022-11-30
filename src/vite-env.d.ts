cc6fe26ec465b20d9765f97aa78f177d79a7cb4d
7y1o <tdesu.webdesign@gmail.com>
2022-11-30 21:38:31 +0300

    Removed styles from main script

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,36 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+/**
+ * Переменная страницы
+ */
+declare interface VariableDef {
+	type: 'string' | 'number' | 'boolean',
+	value: string | number | boolean
+}
+
+/**
+ * Описание компонента
+ */
+export interface ComponentDef {
+
+	/**
+	 * От этой строки зависит, какой компонент будет загружен и виузализирован
+	 */
+	name: string,
+
+	/**
+	 * Пропсы компонента
+	 */
+	props: { [key: string]: any },
+
+	/**
+	 * Применяемые стили компонента
+	 */
+	style: CSSStyleDeclaration,
+
+	/**
+	 * Список дочерних компонентов
+	 */
+	children: ComponentDef[],
+}
